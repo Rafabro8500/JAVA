a24df80105f9df7cb769a75a1a8f1cb0e323310c
Unknown <brandon290697@gmail.com>
2018-04-21 22:46:29 -0600

    Cambio compi

--- Compi/src/compi/Compi.java
+++ Compi/src/compi/Compi.java
@@ -16,6 +16,8 @@ public class Compi {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        
+        System.out.println("No sirve");
     }
     
 }
